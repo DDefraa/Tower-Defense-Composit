73609067e8756104b8a5977307257fe73171dcb1
Francesco <fmdef079@gmail.com>
2023-05-16 21:34:05 +0200

    Primo BackUp

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,34 +1,116 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
+public enum SpawnModes
+{
+    Fixed,
+    Random
+}
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject[] objectPrefabs;
-    public Transform[] pathPoints;
-    public float speed = 5f;
-    public float spawnDelay = 3f;
+    [Header("Settings")]
+    [SerializeField] private SpawnModes spawnMode = SpawnModes.Fixed;
+    [SerializeField] private int enemyCount = 10;
+    [SerializeField] private float delayBtwWaves = 1f;
+
+    [Header("Fixed Delay")]
+    [SerializeField] private float delayBtwSpawns;
+
+    [Header("Random Delay")]
+    [SerializeField] private float minRandomDelay;
+    [SerializeField] private float maxRandomDelay;
+
+    private float _spawnTimer;
+    private int _enemiesSpawned;
+    private int _enemiesRamaining;
+
+    private ObjectPooler _pooler;
+    private Waypoint _waypoint;
 
     private void Start()
     {
-        // Start spawning objects
-        StartCoroutine(SpawnObjects());
+        _pooler = GetComponent<ObjectPooler>();
+        _waypoint = GetComponent<Waypoint>();
+
+        _enemiesRamaining = enemyCount;
     }
 
-    IEnumerator SpawnObjects()
+    private void Update()
     {
-        while (true)
+        _spawnTimer -= Time.deltaTime;
+        if (_spawnTimer < 0)
         {
-            // Spawn objects from the list
-            foreach (GameObject obj in objectPrefabs)
+            _spawnTimer = GetSpawnDelay();
+            if (_enemiesSpawned < enemyCount)
             {
-                GameObject newObj = Instantiate(obj, transform.position, Quaternion.identity);
-
-                // Attach follow path script to the object
-                newObj.AddComponent<FollowPath>().SetPathPoints(pathPoints, speed);
+                _enemiesSpawned++;
+                SpawnEnemy();
             }
-
-            yield return new WaitForSeconds(spawnDelay);
         }
+    }
+
+    private void SpawnEnemy()
+    {
+        GameObject newInstance = _pooler.GetInstanceFromPool();
+        Enemy enemy = newInstance.GetComponent<Enemy>();
+        enemy.Waypoint = _waypoint;
+        enemy.ResetEnemy();
+
+        enemy.transform.localPosition = transform.position;
+        newInstance.SetActive(true);
+    }
+
+    private float GetSpawnDelay()
+    {
+        float delay = 0f;
+        if (spawnMode == SpawnModes.Fixed)
+        {
+            delay = delayBtwSpawns;
+        }
+        else
+        {
+            delay = GetRandomDelay();
+        }
+
+        return delay;
+    }
+
+    private float GetRandomDelay()
+    {
+        float randomTimer = Random.Range(minRandomDelay, maxRandomDelay);
+        return randomTimer;
+    }
+
+    private IEnumerator NextWave()
+    {
+        yield return new WaitForSeconds(delayBtwWaves);
+        _enemiesRamaining = enemyCount;
+        _spawnTimer = 0f;
+        _enemiesSpawned = 0;
+    }
+
+    private void RecordEnemy(Enemy enemy)
+    {
+        _enemiesRamaining--;
+        if (_enemiesRamaining <= 0)
+        {
+            StartCoroutine(NextWave());
+        }
+    }
+
+    private void OnEnable()
+    {
+        Enemy.OnEndReached += RecordEnemy;
+        EnemyHealth.OnEnemyKilled += RecordEnemy;
+    }
+
+    private void OnDisable()
+    {
+        Enemy.OnEndReached -= RecordEnemy;
+        EnemyHealth.OnEnemyKilled -= RecordEnemy;
     }
 }
